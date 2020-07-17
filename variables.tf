a9d7f103efbae131ce165381364388650752f95e
Kyouhei Hayama <khayama@users.noreply.github.com>
2020-07-17 11:35:29 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,9 @@
-variable "project_name" {}
-variable "environment" {}
-
+variable "project_name" {
+  default = "khayama"
+}
+variable "environment" {
+  default = "iks"
+}
 variable "resource_group" {
   default = "khayama-rg"
 }
